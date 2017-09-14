6036d93bbf4f356591a1d80575e65f6ebd8a5e9e
Damian Logghe <damian@timersys.com>
2017-09-14 10:29:42 -0300

    export plugins buttons and premium notice

--- admin/views/header.php
+++ admin/views/header.php
@@ -22,7 +22,13 @@
 				<div class="inside">
 					<p><?php echo sprintf( __( 'If you need support please go to the <a href="%s">support forums</a>', $this->plugin_slug ), 'http://wordpress.org/support/plugin/wpfavs');?></p>
 				</div>
-			</div>				
+			</div>
+			<div class="postbox " style="border: 3px dashed red;">
+				<h3 class="hndle"><span><?php _e( 'PREMIUM VERSION', $this->plugin_slug );?></span></h3>
+				<div class="inside">
+					<p><?php echo sprintf( __( 'Add custom plugins to your list or connect your CodeCanyon account, sign up now on <a href="%s">Wp Favs</a>', $this->plugin_slug ), 'https://wpfavs.com');?></p>
+				</div>
+			</div>
 
 			<div class="postbox ">
 				<h3 class="hndle"><span><?php _e( 'Support Wp Favs!', $this->plugin_slug );?></span></h3>
@@ -34,7 +40,6 @@
 					<p><?php _e( 'Some other ways to support this plugin', $this->plugin_slug );?></p>
 					<ul class="ul-square">
 						<li><a href="http://wordpress.org/support/view/plugin-reviews/wpfavs?rate=5#postform" target="_blank"><?php _e( 'Leave a  &#9733;&#9733;&#9733;&#9733;&#9733; review on WordPress.org', $this->plugin_slug );?></a></li>
-						<li><a href="http://wpfavs" target="_blank"><?php _e( 'Write about Wp Favs on your blog', $this->plugin_slug );?></a></li>
 						<li><a href="http://twitter.com/intent/tweet/?text=I+am+using+Wp+Favs+on+my+WordPress+site.+It%27s+great%21&amp;via=chifliiii&amp;url=http%3A%2F%2Fwordpress.org%2Fplugins%2Fwpfavs%2F" target="_blank"><?php _e( 'Tweet about Wp Favs', $this->plugin_slug );?></a></li>
 						<li><a href="http://wordpress.org/plugins/wpfavs/#compatibility"><?php _e( 'Vote "works" on the WordPress.org plugin page', $this->plugin_slug );?></a></li>
 						<li><?php _e( 'Translate it to your language!', $this->plugin_slug );?></li>
--- admin/views/main.php
+++ admin/views/main.php
@@ -25,6 +25,23 @@
 		<h4>Wordpress.org username</h4>
 		<input type="text" name="wpfav_wp_username" id="wpfav_wp_username" placeholder="<?php _e( 'Insert wordpress.org username to load your favorite plugins', $this->plugin_slug );?>" class="regular-text" value="<?php echo $this->wp_user;?>" />
 		<input class="button-primary wpfav_apikey-button" data-what="wp_username" type="submit" value="<?php _e( 'Load Favorites', $this->plugin_slug );?>" name="Submit">
+		<h4>Other Tools</h4>
+		<?php
+		$all_plugins = get_plugins();
+		if( $all_plugins ){
+			foreach ($all_plugins as $path => $plugin){
+				$installed[] = str_replace(['/','.php'],'',strstr($path,'/'));
+			}
+		}
+		$active = get_option('active_plugins');
+		if( $active ){
+			foreach ($active as $plugin){
+				$activated[] = str_replace(['/','.php'],'',strstr($plugin,'/'));
+			}
+		}
+		?>
+		<a href="https://wpfavs.com/wpfav/create/<?= implode(',',$installed);?>" class="button-primary" target="_blank">Export installed plugins (<?= count($installed);?>)</a>
+		<a href="https://wpfavs.com/wpfav/create/<?= implode(',',$activated);?>" class="button-primary" target="_blank">Export active plugins (<?= count($activated);?>)</a>
 	</form>
 
 	<div id="wplists">
